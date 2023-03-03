0b76f3c84cdb7f8548e66a45838cbf6f7c3adccd
Somtawl <somtawl@yandex.ru>
2023-03-04 00:22:12 +0300

    Добавил обрезку слов и запись в новый архив

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("Введите элементы массива используя знак , в качестве разделителя");
 string[] getElements = Console.ReadLine().Split(",");
 string[] resultArray =  new string[getElements.Length];
+for (int i = 0; i < resultArray.Length; i++)
+{
+    int numberOfSimbols = getElements[i].Length;
+    numberOfSimbols = numberOfSimbols - 3;
+    resultArray[i] = getElements[i].Substring(0, getElements[i].Length - numberOfSimbols);
+}
