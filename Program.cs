ec2bd5aabddf780eca893ffe68bf6755b5e56cfc
Somtawl <somtawl@yandex.ru>
2023-03-04 00:33:41 +0300

    Убрал лишную строку

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@ string[] getElements = Console.ReadLine().Split(",");
 string[] resultArray =  new string[getElements.Length];
 for (int i = 0; i < resultArray.Length; i++)
 {
-    int numberOfSimbols = getElements[i].Length;
-    numberOfSimbols = numberOfSimbols - 3;
+    int numberOfSimbols = getElements[i].Length - 3;
     resultArray[i] = getElements[i].Substring(0, getElements[i].Length - numberOfSimbols);
 }
 for (int j = 0; j < resultArray.Length; j++)
