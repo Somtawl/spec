8268333c7de983835d8555ed6a41d790786c27e9
Somtawl <somtawl@yandex.ru>
2023-03-04 00:21:50 +0300

    Принимаем строку и делим по элементам

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Введите элементы массива используя знак , в качестве разделителя");
+string[] getElements = Console.ReadLine().Split(",");
+string[] resultArray =  new string[getElements.Length];
